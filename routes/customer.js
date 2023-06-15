51318f3c3a183e269aaab5d72c0ba05a22979b71
Ol-create <oluola96@gmail.com>
2023-06-15 13:55:48 +0100

    add customer schema and model

--- routes/customer.js
+++ routes/customer.js
@@ -1,3 +1,21 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const router = mongoose.Router();
 
+const customerSchema = new mongoose.Schema({
+  customer_name: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 50,
+  },
+  phone: {
+    type: String,
+    required: true,
+    min: 7,
+    max: 15,
+  },
+  isGold: Boolean,
+});
 
-
+const Customer = mongoose.model("Customers", customerSchema);
