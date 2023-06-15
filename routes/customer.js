01f6321787bf2fe257490e3302c625bd51903270
Ol-create <oluola96@gmail.com>
2023-06-15 14:58:38 +0100

    api to create new customer

--- routes/customer.js
+++ routes/customer.js
@@ -16,14 +16,29 @@ const customerSchema = new mongoose.Schema({
     min: 7,
     max: 15,
   },
-  isGold: Boolean,
+    isGold: {
+        type: Boolean,
+        default: false
+  }
 });
 
 const Customer = mongoose.model("Customers", customerSchema);
 
+//Get all Customers' data
 router.get('/', async (req, res) => {
     const customers = await Customer.find()
     res.send(customers)
 })
+
+//Create Customer's data
+router.post('/', async (req, res) => {
+    const customer = new Customer({
+        customer_name: req.body.customer_name,
+        phone: req.body.phone
+    })
+    const result = await customer.save()
+   res.send(result)
+})
+
 
 module.exports = router
