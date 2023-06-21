73cebbfd3a969a4de1a1a77e3b4f6a746ba5bffd
Ol-create <oluola96@gmail.com>
2023-06-21 09:00:49 +0100

    add validation to movies

--- models/movies.js
+++ models/movies.js
@@ -17,3 +17,19 @@ const moviesSchema = mongoose.Schema({
   numberInStock: { type: Number, min: 0, max: 10000, required: true },
   dailyRentalRate: { type: Number, min: 0, max: 100, required: true },
 });
+
+const Movies = mongoose.model('movies', moviesSchema);
+
+function validateMovies(genre) {
+  const schema = Joi.object({
+    title: Joi.string().min(3).max(50).trim().required(),
+    genre: Joi.string().required(),
+    numberInStock: Joi.number().min(0).max(1000).trim().required(),
+    dailyRentalRate: Joi.number().min(0).max(100).trim().required(),
+  });
+
+  return schema.validate(genre);
+}
+
+module.exports.Movies = Movies;
+module.exports.validate = validateMovies;
